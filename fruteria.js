259f7ba7e8c70ec30095a6550534fa7bbfbe5c6e
COOPSANJUANBAUTISTA <facturacioncoopsanjuan@gmail.com>
2026-09-02 13:46:20 -0400

    Update fruit images and product details in fruteria.js

--- fruteria.js
+++ fruteria.js
@@ -2,14 +2,23 @@ window.fruteriaInfo = {
     titulo: "Frutería Seleccionada",
     desc: "Disfruta de frutas de temporada dulces, jugosas y llenas de vitaminas, seleccionadas cuidadosamente para llevar la energía del trópico a tu mesa.",
     imagenes: [
-        "https://images.unsplash.com/photo-1610832958506-aa56368176cf?auto=format&fit=crop&q=80&w=800", // Frutas variadas
-        "https://images.unsplash.com/photo-1553279768-865429fa0078?auto=format&fit=crop&q=80&w=800", // Bananos / Cambur
-        "https://images.unsplash.com/photo-1519996521430-025f97749660?auto=format&fit=crop&q=80&w=800"  // Frutas tropicales
+        "./fruteria.png/1281f70d-51dd-41b3-8071-3dcd8a7d74df_removalai_preview.png",
+        "./fruteria.png/13ee1fee-2b5c-4774-b1ef-088148a8be1f_removalai_preview.png",
+        "./fruteria.png/303633c9-e69f-4829-bbfd-602a09f9990f_removalai_preview.png",
+        "./fruteria.png/42cbade8-2712-4e46-b66b-3e7d71bba092_removalai_preview.png",
+        "./fruteria.png/4dd84058-0ad9-4fef-affb-70bc016d131f_removalai_preview.png",
+        "./fruteria.png/60775e8a-f407-4f02-bc2e-2409186e4bf5_removalai_preview.png",
+        "./fruteria.png/636b45ea-aaad-4810-bc1c-1e68035fc8f_removalai_preview.png",
+        "./fruteria.png/7b41197a-1ae8-40a5-8d78-8da3e4e91806_removalai_preview.png",
+        "./fruteria.png/92479290-8e44-4cbb-b4c8-1a8c4f08349c_removalai_preview.png",
+        "./fruteria.png/991d0683-08d8-4e0f-a9d6-ff7cd5d80e3b_removalai_preview.png",
+        "./fruteria.png/cbae61de-a4a0-4a95-a7ae-161625841ce5_removalai_preview.png",
+        "./fruteria.png/ceaabc86-b4ff-4890-931c-14d4bd7e37d8_removalai_preview.png"
     ],
     productos: [
-        { nombre: "Cambur Titiaro", desc: "Dulces y nutritivos.", imagen: "https://images.unsplash.com/photo-1571771894821-ce9b6c11b08e?auto=format&fit=crop&q=80&w=400" },
-        { nombre: "Lechosa", desc: "Excelente para jugos y digestión.", imagen: "https://images.unsplash.com/photo-1517282057600-b1d2c67d740c?auto=format&fit=crop&q=80&w=400" },
-        { nombre: "Piña Regional", desc: "Aromática y jugosa.", imagen: "https://images.unsplash.com/photo-1550258987-190a2d41a8ba?auto=format&fit=crop&q=80&w=400" }
+        { nombre: "Cambur Titiaro", desc: "Dulces y nutritivos.", imagen: "./fruteria.png/1281f70d-51dd-41b3-8071-3dcd8a7d74df_removalai_preview.png" },
+        { nombre: "Lechosa", desc: "Excelente para jugos y digestión.", imagen: "./fruteria.png/13ee1fee-2b5c-4774-b1ef-088148a8be1f_removalai_preview.png" },
+        { nombre: "Piña Regional", desc: "Aromática y jugosa.", imagen: "./fruteria.png/303633c9-e69f-4829-bbfd-602a09f9990f_removalai_preview.png" }
     ]
 };
 
