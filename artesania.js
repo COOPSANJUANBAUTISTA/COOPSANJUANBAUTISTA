b8946d7a631dda34ade55201a539039802748b99
COOPSANJUANBAUTISTA <facturacioncoopsanjuan@gmail.com>
2026-09-07 06:50:43 -0400

    Add artesaniaInfo object with details and images

---
+++ artesania.js
@@ -0,0 +1,8 @@
+window.artesaniaInfo = {
+    titulo: "Artesanía y Tradición",
+    desc: "Piezas elaboradas a mano con dedicación y talento local, reflejando la identidad, creatividad y cultura de nuestra comunidad.",
+    imagenes: [
+        "./artesania.png/artesania1.png",
+        "./artesania.png/artesania2.png"
+    ]
+};
